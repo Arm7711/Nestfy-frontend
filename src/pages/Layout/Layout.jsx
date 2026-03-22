d718fc8f910b86390531fd507f14a29a626c22d0
Arm7711 <armmooooooo@gmail.com>
2026-03-22 23:25:16 +0400

    update site header

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 
+import Header from '../../components/layout/Header/Header';
+
 export default function Layout() {
     return (
-        <main className='antiplace__main'>
-            {<Outlet />}
-        </main>
+        <div className='nestfy__page'>
+            <Header />
+            
+            <main className='antiplace__main'>
+                {<Outlet />}
+            </main>
+        </div>
     )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from
+  '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+// import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr({
+    exportAsDefault: true,
+    svgo: true,
+    svgoConfig: {
+      plugins: [{
+        removeViewBox: false
+      }],
+    },
+  }),
+  // VitePWA({
+  //   registerType: "autoUpdate",
+  //   includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+  //   manifest: false,
+  //   injectManifest: {
+  //     swSrc: 'src/service-worker.js',
+  //   },
+  //   manifestFilename: 'manifest.json' 
+  // })
+
+  ],
+  css: {
+    devSourcemap: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: true,
+  },
+});
