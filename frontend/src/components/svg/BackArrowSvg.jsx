659afdd53b89c8acb52600564372a386cf2c23c7
Arm7711 <armmooooooo@gmail.com>
2026-04-06 19:22:38 +0400

    Delete frontend directory

--- frontend/src/components/svg/BackArrowSvg.jsx
+++
@@ -1,7 +0,0 @@
-export default function BackArrowSvg({ className = 'icon' }) {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 -960 960 960">
-            <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
-        </svg>
-    )
-}
--- frontend/src/pages/NotFound/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import V1Calendar from '../../components/_common/V1Calendar/V1Calendar';
-import CommonSwitch from '../../components/_common/Switchs/CommonSwitch';
-
-export default function NotFound() {
-  return (
-    <V1Calendar/>
-  )
-}
