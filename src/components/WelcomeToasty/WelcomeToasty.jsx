507b99b4aeeb74a7653fde98a2a379542c786c91
Arm7711 <armmoooooo@gmail.com>
2026-04-17 14:36:45 +0400

    fix header animations

--- src/components/WelcomeToasty/WelcomeToasty.jsx
+++ src/components/WelcomeToasty/WelcomeToasty.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BouncyText from '../_common/BouncyText/BouncyText';
+import classNames from 'classnames';
 
 export default function WelcomeToasty() {
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setVisible(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className='welcome__toasty'>
-            <BouncyText
-                key={Date.now()}
-                text='Welcome to Nestfy'
-                loop={true}
-                speed={500}
-                delay={50}
-                lift={4}
-                startAfter={850}
-                mode='rubber'
-            />
+        <div className={classNames('welcome__toasty', { hidden: !visible })}>
+            {visible && (
+                <BouncyText
+                    text='Welcome to Nestfy'
+                    loop={false}
+                    speed={500}
+                    delay={50}
+                    lift={4}
+                    startAfter={850}
+                    mode='rubber'
+                />
+            )}
         </div>
-    )
+    );
 }
